f25f9d6f8a7fe926ea70323d8f388999778d4ebf
Aaron Suggs <aaron@ktheory.com>
2010-01-22 18:19:30 -0500

    Shorted method names

--- lib/juicer/action_view_helper.rb
+++ lib/juicer/action_view_helper.rb
@@ -6,25 +6,23 @@ require 'action_view'
 
 module Juicer::ActionViewHelper
 
-  mattr_accessor :unminified_environments, :use_embedded_images
-  @@unminified_environments = ["development"]
-  @@use_embedded_images = false
+  mattr_accessor :unjuiced_environments, :embed_images
+  @@unjuiced_environments = ["development"]
+  @@embed_images = false
 
   def juiced_tag(asset_path)
     package = Juicer::Package.new(asset_path)
     case package.extension
     when ".css"
-      if should_merge_assets?
-        if should_use_embedded_images?
-          conditional_css_tag(package.embedded_path, package.minified_path)
-        else
-          stylesheet_link_tag(package.minified_path)
-        end
+      if juice_assets? and embed_images?
+        conditional_css_tag(package.embedded_path, package.minified_path)
+      elsif juice_assets?
+        stylesheet_link_tag(package.minified_path)
       else
         stylesheet_link_tag(package.dependencies)
       end
     when ".js"
-      if should_merge_assets?
+      if juice_assets?
         javascript_include_tag(package.minified_path)
       else
         javascript_include_tag(package.dependencies)
@@ -32,12 +30,13 @@ module Juicer::ActionViewHelper
     end
   end
 
-  def should_merge_assets?
-    ! @@unminified_environments.include?(RAILS_ENV)
+  # Write one tag for the minified path, or write tags for each dependency
+  def juice_assets?
+    ! @@unjuiced_environments.include?(RAILS_ENV)
   end
 
-  def should_use_embedded_images?
-    @@use_embedded_images
+  def embed_images?
+    @@embed_images
   end
 
   protected
